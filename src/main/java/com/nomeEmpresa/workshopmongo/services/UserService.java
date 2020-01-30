60ba94af315cf1b9faaedbf41e51aa8cc06ad59d
Renan Galter Brejão <renangalterbrejao@gmail.com>
2020-01-30 13:54:54 -0300

    Inserção de usuário com POST

--- src/main/java/com/nomeEmpresa/workshopmongo/services/UserService.java
+++ src/main/java/com/nomeEmpresa/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nomeEmpresa.workshopmongo.domain.User;
+import com.nomeEmpresa.workshopmongo.dto.UserDTO;
 import com.nomeEmpresa.workshopmongo.repository.UserRepository;
 import com.nomeEmpresa.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -32,5 +33,14 @@ public class UserService {
 		
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		
 	}
 }
